0d9f47ba65b3e0fad9dd3bcd0beda7a00eb3bca4
wenjun.tan@fansentertainment.com <diet4coke>
2019-04-25 13:54:11 -0400

    add test

--- src/test/java/io/zola/controller/InvoiceControllerTest.java
+++ src/test/java/io/zola/controller/InvoiceControllerTest.java
@@ -4,6 +4,7 @@ package io.zola.controller;
 import static io.zola.Constants.INVOICE_NUMBER;
 import static io.zola.Constants.PO_NUMBER;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,6 +19,7 @@ import io.zola.service.context.InvoiceSearchContext;
 import io.zola.service.model.InvoiceTO;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,6 +27,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -46,6 +52,8 @@ public class InvoiceControllerTest {
 
   private JacksonTester<InvoiceDTO> invoiceDTOJacksonTester;
 
+  private JacksonTester<Page<InvoiceDTO>> invoiceDTOPageJacksonTester;
+
   @Before
   public void setUp() {
     JacksonTester.initFields(this, objectMapper);
@@ -65,6 +73,27 @@ public class InvoiceControllerTest {
         .content(invoiceDTOJacksonTester.write(invoiceDTO).getJson())
         .contentType(MediaType.APPLICATION_JSON_VALUE))
         .andExpect(status().isCreated()).andExpect(content().string(invoiceDTOJacksonTester.write(returnedInvoiceDTO).getJson()));
+  }
+
+  @Test
+  public void shouldFailCreateInvoiceRequestWithMissingInfo() throws Exception{
+    InvoiceDTO invoiceDTO = InvoiceDTO.builder().amountCents(10000).dueDate("1988-10-19").poNumber(PO_NUMBER).build();
+    this.mockMvc.perform(post(Routes.INVOICE_CREATE_PATH)
+        .content(invoiceDTOJacksonTester.write(invoiceDTO).getJson())
+        .contentType(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void shouldReturnSearchInvoiceResult() throws Exception{
+    Pageable pageable = PageRequest.of(0, 20);
+    Page<InvoiceTO> searchResult = new PageImpl<>(Collections.emptyList(), pageable, 0);
+
+    InvoiceSearchContext searchContext = InvoiceSearchContext.builder().poNumber("ttttt").pageable(pageable).build();
+    when(invoiceService.search(searchContext)).thenReturn(searchResult);
+
+    this.mockMvc.perform(get(Routes.INVOICE_SEARCH_PATH + "?poNumber=ttttt&pageSize=20&pageNumber=0").contentType(MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(status().isOk());
   }
 
 }
